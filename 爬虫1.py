ef5e7fb71e320bd0b781e5bbe60e879362422bae
oceanmycaptain <32998411+oceanmycaptain@users.noreply.github.com>
2017-10-22 19:00:30 +0800

    Add files via upload

---
+++ 爬虫1.py
@@ -0,0 +1,56 @@
+import urllib.request
+import os
+
+def url_open(ur1):
+    req = urllib.request.Request(url)
+    req.add_headers=('User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.104 Safari/537.36 Core/1.53.3427.400 QQBrowser/9.6.12088.400')
+    response = urllib.request.urlopen(url)
+    html = response.read()
+
+    return html
+
+def get_page(url):
+    html = response.read().decode('utf-8')
+
+    a = html.find('current-comment-page')+ 23
+    b = html.find(']',a)
+
+    return  html[a:b]
+
+
+def find_imgs(url):
+    html = url_open(url).decode('utf-8')
+    img_addrs = []
+
+    a = html.find('img src=')
+
+    while a!= -1:
+        b = html.find('.jpg',a,a+255)
+        if b != -1:
+            img_addrs.append(html[a+9:b+4])
+        else:
+            b = a+9
+        a = html.find('img src=',b)
+    return img_addrs
+
+def save_imgs(folder,img_addrs):
+    for each in img_addrs:
+        filename = each.split('/')[-1]
+        with open(filename,'ab') as f:
+            img = url_open(each)
+            f.write(img)
+
+def download_num(folder = 'ooxx',pages = 10):
+    os.mkdir(folder)
+    os.chdir(folder)
+
+    url = 'http://jandan.net/ooxx'
+    page_num = int(get_page(url))
+
+    for i in range(pages):
+        page_num -=i
+        page_url = url + 'page'+ str(page_num)+'#comments'
+        img_addrs = find_img(page_url)
+        save_imgs(folder,img_addrs)
+if __name__ == '__main__':
+    download_num()
